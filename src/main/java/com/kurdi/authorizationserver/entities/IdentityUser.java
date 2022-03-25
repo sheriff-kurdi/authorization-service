62e44f40673aece4473d58c3fcd43e3bbef27864
kurdi <sheriff.kurdi@gmail.com>
2022-03-25 15:08:50 +0200

    identity user

--- src/main/java/com/kurdi/authorizationserver/entities/IdentityUser.java
+++ src/main/java/com/kurdi/authorizationserver/entities/IdentityUser.java
@@ -1,7 +1,9 @@
 package com.kurdi.authorizationserver.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -12,6 +14,8 @@ import java.util.Set;
 @Data
 @Builder
 @Table(name = "identity_Users")
+@AllArgsConstructor
+@NoArgsConstructor
 public class IdentityUser  implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
