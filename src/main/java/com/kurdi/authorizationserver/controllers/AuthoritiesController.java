3b18795fe69aed4162bd503a385bbed7c107645c
sheriff-kurdi <sheriff.kurdi@gmail.com>
2023-05-21 12:02:11 +0300

    ref

--- src/main/java/com/kurdi/authorizationserver/controllers/AuthoritiesController.java
+++ src/main/java/com/kurdi/authorizationserver/controllers/AuthoritiesController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("Authorities/")
+@RequestMapping("authorities/")
 public class AuthoritiesController {
     @Autowired
     AuthService authService;
